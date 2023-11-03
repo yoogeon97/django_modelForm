60f4334a3cf1a1cb6adcc612fe40c79c0d9f6666
yoogeon <yoogeon97@gmail.com>
2023-11-03 15:12:36 +0900

    게시물 만들기

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Article
+from .forms import ArticleForm
 
 # Create your views here.
+def index(request):
+    articles = Article.objects.all()
+
+    context = {
+        'articles' : articles
+    }
+
+    return render(request, 'index.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:index')
+    else: 
+        form = ArticleForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'form.html', context)
+
+def delete(request, id):
+    article = Article.objects.get(id=id)
+    article.delete()
+
+    return redirect('articles:index')
+
+def update(request, id):
+    article = Article.objects.get(id=id)
+
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        article = Article.objects.get(id=id)
+        form = ArticleForm(instance=article)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'form.html', context)
